c77c55c331f8dc1c196ff7a55a2719c09dea75be
jiyoonhur <jiyoon1156@gmail.com>
2021-03-03 15:31:21 +0900

    [ADD] front boilerplate

---
+++ front/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['js', 'jsx'] }],
+    'no-console': ['off'],
+    'react/jsx-props-no-spreading': ['warn'],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+      },
+    },
+  },
+};
