37078ab2bf42cf7ac88b456b797a0b725a9a9687
jiyoonhur <jiyoon1156@gmail.com>
2021-03-23 15:40:12 +0900

    [FEAT-Server] Add Routers

---
+++ server/routes/getCoupon.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const pool = require('../models/DbConnection');
+
+router.get('', async (req, res) => {
+  try {
+		const result = await pool.query('SELECT * FROM coupon');
+    res.status(200).json({ result: result[0] });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
